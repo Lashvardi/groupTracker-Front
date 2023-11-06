58f83f15d989695571cfcf6948829f478c0bc656
Lasha <lashakakashvili114@gmail.com>
2023-11-06 13:59:00 +0400

    Add font styles and update layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Route,
+  Router,
+  UrlSegment,
+} from '@angular/router';
 import { Component } from '@angular/core';
 import { AuthService } from './modules/auth/extensions/auth.service';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +15,12 @@ import { AuthService } from './modules/auth/extensions/auth.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(public _authService: AuthService) {}
+  currentRoute!: string;
+
+  constructor(
+    public _authService: AuthService,
+    private _activatedRoute: ActivatedRoute
+  ) {}
+
+  // get the current route data
 }
