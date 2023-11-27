7f8e724d8ba4c37075fc7a0a9f52eecb51281672
Lasha <lashakakashvili114@gmail.com>
2023-11-27 15:33:50 +0400

    Fix routing and update group card component

--- src/app/modules/dashboard/export/dashboard-routing.module.ts
+++ src/app/modules/dashboard/export/dashboard-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
   {
     path: 'Calendar',
     component: CalendarComponent,
-    canActivate: [NoAuthGuard],
   },
 ];
 
--- src/app/shared/group-card/group-card.component.ts
+++ src/app/shared/group-card/group-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-group-card',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./group-card.component.scss']
 })
 export class GroupCardComponent {
-
+  @Input() groupName: string = '';
+  @Input() groupGrade: string = '';
+  @Input() groupSubject: string = '';
+  @Input() groupCompany: string = '';
+  @Input() groupAuditorium: string = '';
+  @Input() groupTime: string = '';
+  @Input() CurrentLecture: string = '';
+  @Input() groupDay: string = '';
+  @Input() groupProgress: string = '';
 }
